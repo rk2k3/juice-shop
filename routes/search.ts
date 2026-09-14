aac0b571981e60a560a4198739dedd42247d8035
ron <ronkoh2511@gmail.com>
2026-09-14 11:41:41 +0800

    fix routes

--- routes/search.ts
+++ routes/search.ts
@@ -21,8 +21,8 @@ export function searchProducts () {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     models.sequelize.query(
-      `SELECT * FROM Products WHERE ((name LIKE '%:criteria%' OR description LIKE '%:criteria%') AND deletedAt IS NULL) ORDER BY name`,
-      { replacements: { criteria } }
+      `SELECT * FROM Products WHERE ((name LIKE :criteria OR description LIKE :criteria) AND deletedAt IS NULL) ORDER BY name`,
+      { replacements: { criteria: `%${criteria}%` } }
     )
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
